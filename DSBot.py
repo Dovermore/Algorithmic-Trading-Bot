e1b34bea7635c3cce7707542629be4f988b55904
jundaaa <42367388+jundaaa@users.noreply.github.com>
2018-08-14 15:52:02 +1000

    Add name Lee Jun Da

--- DSBot.py
+++ DSBot.py
@@ -7,7 +7,7 @@ from fmclient import Agent, OrderSide, Order, OrderType
 import time
 
 # Group details
-GROUP_MEMBERS = {"908525": "Zhuoqun Huang", "836389": "Nikolai Price"}
+GROUP_MEMBERS = {"908525": "Zhuoqun Huang", "836389": "Nikolai Price", "888086": "Lee Jun Da"}
 
 # ------ Add a variable called DS_REWARD_CHARGE -----
 # Dependent on actual task
