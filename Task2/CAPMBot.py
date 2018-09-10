f0fc25b661f55b29c25eac62168f36cb86ec83f1
jundaaa <42367388+jundaaa@users.noreply.github.com>
2018-09-10 18:43:06 +1000

    Getting started
    Some are from previous project
    Created market_ids to collect information abour the market the securities are in and put them into a dictionary

--- Task2/CAPMBot.py
+++ Task2/CAPMBot.py
@@ -6,13 +6,73 @@ Student Name (ID): Zhuoqun Huang (908525)
                    Lee Jun Da    (888086)
 """
 
-
-from fmclient import Agent
-from fmclient import Order, OrderSide, OrderType
+from enum import Enum
+from fmclient import Agent, OrderSide, Order, OrderType
+import copy
+import time
+from collections import defaultdict as dd
 
 # Group details
 GROUP_MEMBERS = {"908525": "Zhuoqun Huang", "836389": "Nikolai Price",
                  "888086": "Lee Jun Da"}
+
+# <For debugging only>
+import inspect
+
+INIT_STACK = 12
+STACK_DIF = 10
+BASE_LEN = 79
+# </For debugging only>
+
+# ------ Add a variable called DS_REWARD_CHARGE -----
+# Dependent on actual task
+DS_REWARD_CHARGE = 500
+MAX_REWARD_UNIT = 5
+
+# The unit to place order
+ORDER_UNIT = 1
+
+# The MAGIC cancel number
+MAGIC_MM_CANCEL_CYCLE = 10
+
+# Dictionary to store letters in representation of a certain OrderType
+# and OrderSide for reference of orders
+ORDER_TYPE_TO_CHAR = {
+    OrderType.LIMIT: "L",
+    OrderType.CANCEL: "M"
+}
+ORDER_SIDE_TO_CHAR = {
+    OrderSide.BUY: "B",
+    OrderSide.SELL: "S"
+}
+ORDER_AVAILABILITY_TEMPLATE = {
+    # Verifying units within 5 should be the responsibility of mm and reactive
+    # function, not order verifier.
+    "cash_available": None,     # Is cash enough to place this order
+    "unit_available": None,     # Is unit enough to place this order
+}
+SEPARATION = "-"  # for most string separation
+
+
+# Enum for the roles of the bot
+class Role(Enum):
+    BUYER = 0
+    SELLER = 1
+
+
+# Let us define another enumeration to deal with the type of bot
+class BotType(Enum):
+    MARKET_MAKER = 0
+    REACTIVE = 1
+
+
+# Status of current order if there is any
+class OrderStatus(Enum):
+    CANCEL = -1        # Cancelled, turns INACTIVE when accepted
+    INACTIVE = 0       # None/Completed/Rejected
+    MADE = 1           # Made, turns PENDING when sent
+    PENDING = 2        # Waiting to be accepted in the order book
+    ACCEPTED = 3       # Accepted in the order book
 
 
 class CAPMBot(Agent):
@@ -31,12 +91,46 @@ class CAPMBot(Agent):
         self._payoffs = {}
         self._risk_penalty = risk_penalty
         self._session_time = session_time
-        self._market_ids = {} 
+        self._market_ids = {}
+
+        # TODO use time to change the type of bot to be more
+        # TODO     aggressive when almost end we we have not reach target
+        self._bot_type = BotType.REACTIVE
+
+        # TBD later
+        self._role = None
+
+        # This member variable take advantage of only one order at a time
+        # --------------------------------------------------------------------
+        # Stores active order currently in the order book
+        self._active_order = None
+        self._order_status = OrderStatus.INACTIVE
+        self._order_availability = copy.copy(ORDER_AVAILABILITY_TEMPLATE)
+        # --------------------------------------------------------------------
+
+        # Iterations since Market Maker order made
+        self._mm_order_cycle = 0
+
+        # Stores any inactive order that has been rejected
+        self._inactive_order = []
+
+        # Additional information, not particularly useful, but helps with
+        # Verifying when calling `received_holdings`.
+        self.mine_orders = None
 
     def initialised(self):
-        for market_id, market_info in self.markets.items():
-            security = market_info["item"]
-            description = market_info["description"]
+        """
+        Initialise by looking at the requirements of the market,
+        collects data regarding the market to be traded in and their
+        respective payoff
+        """
+        self.inform("Initialised, examining markets available")
+        for market_id, market_dict in self.markets.items():
+            self.inform(self._str_market(market_dict))
+            security = market_dict["item"]
+            security_id = market_dict["id"]
+            description = market_dict["description"]
+            self._market_ids[security] = security_id
             self._payoffs[security] = [int(a) for a in description.split(",")]
 
     def get_potential_performance(self, orders):
@@ -51,7 +145,8 @@ class CAPMBot(Agent):
 
     def is_portfolio_optimal(self):
         """
-        Returns true if the current holdings are optimal (as per the performance formula), false otherwise.
+        Returns true if the current holdings are optimal
+        (as per the performance formula), false otherwise.
         :return:
         """
         pass
@@ -63,6 +158,24 @@ class CAPMBot(Agent):
         pass
 
     def received_order_book(self, order_book, market_id):
+
+        self._line_break_inform(inspect.stack()[0][3],
+                                length=BASE_LEN + INIT_STACK * STACK_DIF -
+                                self.get_stack_size() * STACK_DIF)
+        self.inform("received order book from %d" % market_id)
+
+        try:
+            pass
+
+        except Exception as e:
+            self._exception_inform(e, inspect.stack()[0][3])
+
+    def _get_bid_ask_price(self, orders):
+        """
+        Get the best bid and best ask price of the market
+        :param orders: Orders from the order book
+        :return: Best bid and best ask
+        """
         pass
 
     def received_completed_orders(self, orders, market_id=None):
@@ -77,6 +190,100 @@ class CAPMBot(Agent):
     def run(self):
         self.initialise()
         self.start()
+
+    def _line_break_inform(self, msg="", char="-",
+                           length=BASE_LEN, width=BASE_LEN):
+        """
+        Simply inform a line break with certain character
+        :param char:   The character to be repeated
+        :param length: The number of repetition char would be repeated
+        :param width:  The least width of line (symmetric space padding)
+        """
+        len_char = (length - len(msg)) // len(char)
+        char_left = len_char // 2
+        char_right = len_char - char_left
+        len_space = width - length
+        if len_space < 0:
+            len_space = 0
+        space_left = len_space // 2
+        space_right = len_space - space_left
+
+        self.inform(" " * space_left + "".join([char] * char_left) +
+                    msg + "".join([char] * char_right) + " " * space_right)
+
+    def _exception_inform(self, msg, fn_name, addition=""):
+        """
+        Show the exception message with function name
+        :param msg: exception to inform
+        """
+        assert isinstance(msg, Exception), ("msg %s is not an exception"
+                                            % str(msg))
+        if len(addition) > 0:
+            addition = "addition, " + addition
+        self.warning("^^^Exception in function %s^^^:"
+                     "msg: %s%s" % (fn_name, str(msg), addition)
+                     )
+
+    @staticmethod
+    def _str_market(market):
+        """
+        This is a staticmethod that returns the string representation of detail
+        of a market
+        :param market: Dictionary of a market to be turned into string
+        """
+        try:
+            return ("Market: %d\n"
+                    "                       Minimum: %3d\n"
+                    "                       Maximum: %3d\n"
+                    "                       Tick   : %3d\n"
+                    "                       Name   : %s\n"
+                    "                       Item   : %s\n"
+                    "                       Describ: %s\n" %
+                    (market["id"], market["minimum"], market["maximum"],
+                     market["tick"], market["name"], market["item"],
+                     market["description"]))
+        except Exception as e:
+            return e
+
+    @staticmethod
+    def str_order(order):
+        """
+        This function return string representation of the detail of orders
+        :param order: object of Order class of fmclient
+        """
+        try:
+            if order is not None:
+                return ("Order Detail\n"
+                        "                       Price: %d\n"
+                        "                       Units: %d\n"
+                        "                       Type:  %s\n"
+                        "                       Side:  %s\n"
+                        "                       Ref:   %s\n"
+                        "                       Id:    %d\n"
+                        % (order.price,
+                           order.units,
+                           ORDER_TYPE_TO_CHAR[order.type],
+                           ORDER_SIDE_TO_CHAR[order.side],
+                           order.ref if order.ref else "No Ref",
+                           order.id if order.id else -1))
+            else:
+                return ("Order Detail\n"
+                        "      Order is None")
+        except Exception as e:
+            return e
+
+    # Used for visualisation of function call as stacks, that it's easier to
+    # trace through functions
+    @staticmethod
+    def get_stack_size():
+        """
+        Get stack size for caller's frame.
+        %timeit len(inspect.stack())
+        8.86 ms ± 42.5 µs per loop (mean ± std. dev. of 7 runs, 100 loops each)
+        %timeit get_stack_size()
+        4.17 µs ± 11.5 ns per loop (mean ± std. dev. of 7 runs, 100000 loops each)
+        """
+        return len(inspect.stack())
 
 
 if __name__ == "__main__":
