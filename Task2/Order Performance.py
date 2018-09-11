b63624468eca717008cd0c2224558a77269a0d48
jundaaa <42367388+jundaaa@users.noreply.github.com>
2018-09-11 20:32:53 +1000

    generating orders

--- Task2/Order Performance.py
+++ Task2/Order Performance.py
@@ -1,11 +1,34 @@
 import time
 import timeit
+from fmclient import Order, OrderSide, OrderType
+import random
+
+random.seed(100)
+
+order_side = {
+    0: OrderSide.BUY,
+    1: OrderSide.SELL
+}
+
+order_list = []
+
+for x in range(10):
+    units = random.randint(1, 5)
+    side = order_side[random.randint(0, 1)]
+    price = random.randint(0, 1000)//5*5
+    order_to_make = Order(price, units, OrderType.LIMIT, side, 250)
+    order_list.append(order_to_make)
+
+for i in range(10):
+    print(order_list[i])
 
 t0 = time.time()
+order = None
 
 
-def best_orders():
-    pass
+def best_orders(orders):
+    for order in orders:
+        pass
 
 
 t1 = time.time()
